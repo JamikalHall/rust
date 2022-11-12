6876598b502b5f74717471fd58ceb1d40f35f85d
Mike <jamikal.hall@gmail.com>
2022-11-12 13:49:18 -0600

    hello world and hello cargo!

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-9a380041da3a0e62.d
@@ -0,0 +1,5 @@
+c:\Users\jamik\Desktop\coffeeapp\rust\projects\hello_cargo\target\debug\deps\hello_cargo-9a380041da3a0e62.rmeta: src\main.rs
+
+c:\Users\jamik\Desktop\coffeeapp\rust\projects\hello_cargo\target\debug\deps\hello_cargo-9a380041da3a0e62.d: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\jamik\Desktop\coffeeapp\rust\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\jamik\Desktop\coffeeapp\rust\projects\hello_cargo\src\main.rs
